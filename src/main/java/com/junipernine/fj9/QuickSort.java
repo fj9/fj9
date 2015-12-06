6dd055772e2f28b2e76febd19808060d0d3bf34c
ccfreya <freya.junipernine@gmail.com>
2015-12-06 22:57:42 +0000

    Sorting problems,

---
+++ src/main/java/com/junipernine/fj9/QuickSort.java
@@ -0,0 +1,46 @@
+package com.junipernine.fj9;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created on 06/12/2015.
+ */
+public class QuickSort {
+
+    public List<String> sort(List<String> strings) {
+        if (strings.size() > 1) {
+            return sort(0, strings.size() - 1, strings);
+        } else {
+            return strings;
+        }
+
+    }
+
+    private List<String> sort(int lowerIndex, int higherIndex, List<String> strings) {
+        if (higherIndex - lowerIndex > 0) {
+            String partitionItem = strings.get(lowerIndex);
+
+            int partitionItemIndex = lowerIndex;
+            int left = lowerIndex;
+            int right = higherIndex;
+            while (left < right) {
+                while (left <= higherIndex && strings.get(left).compareTo(partitionItem) <= 0) {
+                    left++;
+                }
+                if (left < right) {
+                    Collections.swap(strings, left, right);
+                    right--;
+                }
+            }
+            Collections.swap(strings, lowerIndex, right);
+            sort(lowerIndex, right - 1, strings);
+            sort(right, higherIndex, strings);
+        }
+        return strings;
+    }
+
+
+}
